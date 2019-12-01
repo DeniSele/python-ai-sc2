6b945e02b5eb85b4fa6750577c15ec36fb1558e3
DeniSele <denis.seledtsow@gmail.com>
2019-12-01 20:02:24 +0300

    The first prototype of an autonomous player.

---
+++ bots_controller.py
@@ -0,0 +1,256 @@
+import sc2
+from sc2 import Difficulty, Race, maps, run_game, position
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, OBSERVER, ROBOTICSFACILITY, SCV
+from sc2.player import Bot, Computer
+import sys
+import random
+import neural_network
+import numpy as np
+import pickle
+import math
+
+
+class CustomBot(sc2.BotAI):
+    def __init__(self):
+        self.SECONDS_PER_MINUTE = 60
+        self.MAX_WORKERS = 50
+
+        # {UNIT_TAG: LOCATION}
+        self.scouts_and_spots = {}
+        self.end_game = False
+
+        self.GLOBAL_TIME = 0
+        self.PLAYER_ID = -1
+        self.GENERATION = 0
+        self.ACTION_VECTOR = [0] * 3
+
+        if len(sys.argv) > 2:
+            self.PLAYER_ID = sys.argv[1]
+            self.GENERATION = sys.argv[2]
+            f = open("weights\\generation_" + str(self.GENERATION) +
+                     "_weights_" + str(self.PLAYER_ID) + ".pkl", "rb")
+            self.PLAYER_BRAIN = pickle.load(f)
+            f.close()
+
+    def on_end(self, game_result):
+        print('---End of the game---')
+        f = open("results\\generation_" + str(self.GENERATION) + "_player_" +
+                 str(self.PLAYER_ID) + ".txt", "w")
+        if not self.end_game:
+            f.write(str(game_result) + "\n")
+        else:
+            f.write("Result.Defeat\n")
+
+        f.write(str(self.ACTION_VECTOR) + "\n")
+
+    async def on_step(self, iteration):
+        self.GLOBAL_TIME = (self.state.game_loop / 22.4)
+        await self.build_scout()
+        await self.scout()
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
+        await self.attack()
+
+    async def build_scout(self):
+        if len(self.units(OBSERVER)) < (self.GLOBAL_TIME / 100):
+            if len(self.units(OBSERVER)) < 10:
+                for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
+                    if self.can_afford(OBSERVER) and self.supply_left > 0:
+                        await self.do(rf.train(OBSERVER))
+
+    def random_location_variance(self, enemy_start_location):
+        x = enemy_start_location[0]
+        y = enemy_start_location[1]
+
+        x += random.randrange(-5, 5)
+        y += random.randrange(-5, 5)
+
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+        if x > self.game_info.map_size[0]:
+            x = self.game_info.map_size[0]
+        if y > self.game_info.map_size[1]:
+            y = self.game_info.map_size[1]
+
+        go_to = position.Point2(position.Pointlike((x, y)))
+        return go_to
+
+    async def scout(self):
+        self.expand_dis_dir = {}
+        for el in self.expansion_locations:
+            distance_to_enemy_start = el.distance_to(
+                self.enemy_start_locations[0])
+            self.expand_dis_dir[distance_to_enemy_start] = el
+        self.ordered_exp_distances = sorted(k for k in self.expand_dis_dir)
+
+        existing_ids = [unit.tag for unit in self.units]
+        to_be_removed = []
+        for noted_scount in self.scouts_and_spots:
+            if noted_scount not in existing_ids:
+                to_be_removed.append(noted_scount)
+
+        for scount in to_be_removed:
+            del self.scouts_and_spots[scount]
+
+        if len(self.units(ROBOTICSFACILITY).ready) == 0:
+            unit_type = PROBE
+            unit_limit = 1
+        else:
+            unit_type = OBSERVER
+            unit_limit = 15
+
+        assign_scout = True
+
+        if unit_type == PROBE:
+            for unit in self.units(PROBE):
+                if unit.tag in self.scouts_and_spots:
+                    assign_scout = False
+
+        if assign_scout:
+            if len(self.units(unit_type).idle) > 0:
+                for obs in self.units(unit_type).idle[:unit_limit]:
+                    if obs.tag not in self.scouts_and_spots:
+                        for dist in self.ordered_exp_distances:
+                            try:
+                                location = self.expand_dis_dir[dist]
+                                active_locations = [
+                                    self.scouts_and_spots[k]
+                                    for k in self.scouts_and_spots
+                                ]
+
+                                if location not in active_locations:
+                                    if unit_type == PROBE:
+                                        for unit in self.units(PROBE):
+                                            if unit.tag in self.scouts_and_spots:
+                                                continue
+                                    await self.do(obs.move(location))
+                                    self.scouts_and_spots[obs.tag] = location
+                                    break
+                            except Exception as e:
+                                #print(str(e))
+                                pass
+        for obs in self.units(unit_type):
+            if obs.tag in self.scouts_and_spots:
+                if obs in [probe for probe in self.units(PROBE)]:
+                    await self.do(
+                        obs.move(
+                            self.random_location_variance(
+                                self.scouts_and_spots[obs.tag])))
+
+    async def build_workers(self):
+        if len(self.units(NEXUS)) * 16 > len(self.units(PROBE)):
+            if len(self.units(PROBE)) < self.MAX_WORKERS:
+                for nexus in self.units(NEXUS).ready.noqueue:
+                    if self.can_afford(PROBE):
+                        await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses.first)
+
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0,
+                                                           vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+
+    async def expand(self):
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def offensive_force_buildings(self):
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+
+            if self.units(
+                    GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(
+                        CYBERNETICSCORE
+                ) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+
+            elif len(self.units(GATEWAY)) < 1:
+                if self.can_afford(
+                        GATEWAY) and not self.already_pending(GATEWAY):
+                    await self.build(GATEWAY, near=pylon)
+
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(ROBOTICSFACILITY)) < 1:
+                    if self.can_afford(
+                            ROBOTICSFACILITY
+                    ) and not self.already_pending(ROBOTICSFACILITY):
+                        await self.build(ROBOTICSFACILITY, near=pylon)
+
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(STARGATE)) < (
+                        self.GLOBAL_TIME / self.SECONDS_PER_MINUTE):
+                    if self.can_afford(
+                            STARGATE) and not self.already_pending(STARGATE):
+                        await self.build(STARGATE, near=pylon)
+
+    async def build_offensive_force(self):
+        for sg in self.units(STARGATE).ready.noqueue:
+            if self.can_afford(VOIDRAY) and self.supply_left > 0:
+                await self.do(sg.train(VOIDRAY))
+
+    async def attack(self):
+        target = False
+
+        if self.GLOBAL_TIME % 5 == 0:
+            if self.GLOBAL_TIME > 1500:
+                self.end_game = True
+
+            if len(self.units(VOIDRAY).noqueue) > 0:
+                if not self.end_game:
+                    choice = neural_network.predict_solo_output(self.PLAYER_BRAIN, [
+                        self.GLOBAL_TIME / 100.0,
+                        len(self.units(VOIDRAY).noqueue),
+                        len(self.known_enemy_units.exclude_type([SCV])),
+                        len(self.known_enemy_units.of_type([SCV]))
+                    ])
+                else:
+                    choice = random.randrange(0, 2)
+
+                self.ACTION_VECTOR[choice] += 1
+
+                if choice == 0:
+                    return
+                elif choice == 1:
+                    if len(self.known_enemy_units) > 0:
+                        target = self.known_enemy_units.closest_to(
+                            random.choice(self.units(NEXUS)))
+                elif choice == 2:
+                    if len(self.known_enemy_structures) > 0:
+                        target = random.choice(self.known_enemy_structures)
+                        for vr in self.units(VOIDRAY).noqueue:
+                            await self.do(vr.attack(target))
+                    else:
+                        target = self.enemy_start_locations[0]
+
+                if target:
+                    for vr in self.units(VOIDRAY).idle:
+                        await self.do(vr.attack(target))
+
+
+run_game(
+    maps.get("AbyssalReefLE"),
+    [Bot(Race.Protoss, CustomBot()),
+     Computer(Race.Terran, Difficulty.Medium)],
+    realtime=False)
---
+++ training_launcher.py
@@ -0,0 +1,140 @@
+import sys
+import subprocess
+import numpy
+import evo_algorithm
+import neural_network
+import pickle
+
+
+# When you start for the first time or want to start all over again:
+#   - Set USE_SAVED_WEIGHTS to False
+#   - Set LOAD_GENERATION_NUMBER to 0
+#
+# To use existing data:
+#   - Set USE_SAVED_WEIGHTS to True
+#   - Set LOAD_GENERATION_NUMBER to N
+#   N - number of generation files to be used
+USE_SAVED_WEIGHTS = False
+LOAD_GENERATION_NUMBER = 0
+
+# Population size
+sol_per_pop = 8
+
+# Half the population to break it up into pieces
+half_part_of_sol = 4
+
+# Mating pool size (number of parents)
+num_parents_mating = 4
+
+# Number of generations
+num_generations = 2
+
+# Mutation percent
+mutation_percent = 10
+
+# The number of neurons in the input layer
+input_size = 4
+
+
+initial_pop_weights = []
+if not USE_SAVED_WEIGHTS:
+    # Creating the initial population.
+
+    print("Create a new population")
+    for curr_sol in numpy.arange(0, sol_per_pop):
+        HL1_neurons = 6
+        input_HL1_weights = numpy.random.uniform(
+            low=-0.1, high=0.1, size=(input_size, HL1_neurons))
+        HL2_neurons = 5
+        HL1_HL2_weights = numpy.random.uniform(
+            low=-0.1, high=0.1, size=(HL1_neurons, HL2_neurons))
+        output_neurons = 3
+        HL2_output_weights = numpy.random.uniform(
+            low=-0.1, high=0.1, size=(HL2_neurons, output_neurons))
+
+        initial_pop_weights.append(
+            numpy.array(
+                [input_HL1_weights, HL1_HL2_weights, HL2_output_weights]))
+else:
+    # Get the latest saved version of the data, based on LOAD_GENERATION_NUMBER
+    for curr_sol in numpy.arange(0, sol_per_pop):
+        f = open("weights\\generation_" + str(LOAD_GENERATION_NUMBER) +
+                 "_weights_" + str(curr_sol) + ".pkl", "rb")
+        new_weight = pickle.load(f)
+        initial_pop_weights.append(new_weight)
+        f.close()
+
+pop_weights_mat = numpy.array(initial_pop_weights)
+pop_weights_vector = evo_algorithm.mat_to_vector(pop_weights_mat)
+
+for generation in range(LOAD_GENERATION_NUMBER,
+                        num_generations + LOAD_GENERATION_NUMBER):
+    print("Generation: ", generation)
+
+    # Converting the solutions from being vectors to matrices.
+    pop_weights_mat = evo_algorithm.vector_to_mat(pop_weights_vector, pop_weights_mat)
+
+    # Measuring the fitness of each chromosome in the population.
+    fitness = [0] * sol_per_pop
+
+    # Split the launch in half to provide enough power
+    for j in range(2):
+        procs = []
+        for i in range(j * half_part_of_sol, half_part_of_sol * (j + 1)):
+            print("i: ", i)
+            f = open("weights\\generation_" + str(generation) + "_weights_" +
+                     str(i) + ".pkl", "wb")
+            pickle.dump(pop_weights_mat[i], f)
+            f.close()
+            proc = subprocess.Popen([
+                sys.executable, 'bots_controller.py',
+                str(i),
+                str(generation)
+            ])
+            procs.append(proc)
+
+        for proc in procs:
+            proc.wait()
+
+    # Fitness calculation based on results
+    for i in range(sol_per_pop):
+        f = open("results\\generation_" + str(generation) + "_player_" +
+                 str(i) + ".txt", "r")
+        result_str = f.readline()
+
+        if result_str == "Result.Victory\n":
+            fitness[i] = 1
+        else:
+            fitness[i] = 0
+
+    print("Fitness")
+    print(fitness)
+
+    # Selecting the best parents in the population for mating.
+    parents = evo_algorithm.select_mating_pool(pop_weights_vector, fitness.copy(),
+                                    num_parents_mating)
+
+    # Generating next generation using crossover.
+    offspring_crossover = evo_algorithm.crossover(
+        parents,
+        offspring_size=(pop_weights_vector.shape[0] - parents.shape[0],
+                        pop_weights_vector.shape[1]))
+
+    # Adding some variations to the offsrping using mutation.
+    offspring_mutation = evo_algorithm.mutation(
+        offspring_crossover, mutation_percent=mutation_percent)
+
+    # Creating the new population based on the parents and offspring.
+    pop_weights_vector[0:parents.shape[0], :] = parents
+    pop_weights_vector[parents.shape[0]:, :] = offspring_mutation
+
+# Saving the final results for this launch
+print("Final weights for generation",
+      (num_generations + LOAD_GENERATION_NUMBER), "saving...")
+for i in range(0, sol_per_pop):
+    print("saved weight: ", i)
+    f = open(
+        "weights\\generation_" + str(num_generations + LOAD_GENERATION_NUMBER)
+        + "_weights_" + str(i) + ".pkl", "wb")
+    pickle.dump(pop_weights_mat[i], f)
+    f.close()
